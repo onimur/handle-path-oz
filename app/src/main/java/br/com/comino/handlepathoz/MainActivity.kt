0095dd47e885f0ae0b51f364b04cae9181d66c4b
Murillo Comino <murillo_comino@hotmail.com>
2020-06-03 18:51:14 -0300

    Add - Copyright for the entire project

--- app/src/main/java/br/com/comino/handlepathoz/MainActivity.kt
+++ app/src/main/java/br/com/comino/handlepathoz/MainActivity.kt
@@ -1,7 +1,19 @@
+/*
+ *
+ *  * Created by Murillo Comino on 03/06/20 18:49
+ *  * Github: github.com/MurilloComino
+ *  * StackOverFlow: pt.stackoverflow.com/users/128573
+ *  * Email: murillo_comino@hotmail.com
+ *  *
+ *  * Copyright (c) 2020.
+ *  * Last modified 03/06/20 18:49
+ *
+ */
+
 package br.com.comino.handlepathoz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
